650939ddcffcb4c1fdf60b4f4cb9c2bfb0b85d5d
Clayton Adams <clayadams52@gmail.com>
2020-12-23 23:53:11 -0500

    Added Elements to the Vault Page

--- Pages/VaultPage.cs
+++ Pages/VaultPage.cs
@@ -11,8 +11,63 @@ namespace BitwardenAutomation.Pages
         IWebElement lnkSend => Driver.FindElement(By.LinkText("Send"));
         IWebElement lnkTools => Driver.FindElement(By.LinkText("Tools"));
         IWebElement lnkSettings => Driver.FindElement(By.LinkText("Settings"));
+        IWebElement lnkAllItems => Driver.FindElement(By.LinkText("All Items"));
+        IWebElement lnkFavorites => Driver.FindElement(By.LinkText("Favorites"));
+        IWebElement lnkTrash => Driver.FindElement(By.LinkText("Trash"));
+        IWebElement lnkLogin => Driver.FindElement(By.LinkText("Login"));
+        IWebElement lnkCard => Driver.FindElement(By.LinkText("Card"));
+        IWebElement lnkIdentity => Driver.FindElement(By.LinkText("Identity"));
+        IWebElement lnkSecureNote => Driver.FindElement(By.LinkText("Secure Note"));
 
+        public void CickSend()
+        {
+            lnkSend.Click();
+        }
 
+        public void ClickTools()
+        {
+            lnkTools.Click();
+        }
+
+        public void ClickSettings()
+        {
+            lnkSettings.Click();
+        }
+
+        public void ClickAllItems()
+        {
+            lnkAllItems.Click();
+        }
+
+        public void ClickFavorites()
+        {
+            lnkFavorites.Click();
+        }
+
+        public void clickTrash()
+        {
+            lnkTrash.Click();
+        }
+
+        public void ClickLogin()
+        {
+            lnkLogin.Click();
+        }
+
+        public void ClickCard()
+        {
+            lnkCard.Click();
+        }
+
+        public void ClickIdentity()
+        {
+            lnkIdentity.Click();
+        }
+
+        public void ClickSecureNote()
+        {
+            lnkSecureNote.Click();
+        }
 
         public String getURL()
         {
--- Tests.cs
+++ Tests.cs
@@ -28,37 +28,25 @@ namespace BitwardenAutomation
             LoginPage loginPage = new LoginPage();
             HintPage hintPage = new HintPage();
 
-            //Change to implicent wait and put into method
+            //Change to implicint Wait
             Thread.Sleep(3000);
-
             loginPage.ClickHint();
             hintPage.enterEmail("bitwarden@mailsac.com");
             hintPage.clickSubmit();
             hintPage.clickCancel();
-            //TODO Check email 
-        }
-
-        [Test]
-        public void checkMail()
-        {
-            DriverHelper.InitBrowser();
             Driver.Navigate().GoToUrl("https://mailsac.com/login");
-
             MailSac mailSac = new MailSac();
             mailSac.LogIn("mad5226", "password01");
             Thread.Sleep(3000);
             mailSac.clickMailBox();
-            mailSac.IsEmailReceived("Your Master Password Hint");
-
+            bool emailRecieved = mailSac.IsEmailReceived("Your Master Password Hint");
             //TODO: Delete email after confirming
-
-            //  Assert.That(mailSac.IsEmailReceived, Is.True, "Can't find the email");
+            Assert.That(emailRecieved, Is.True, "Can't find the email");
         }
 
         [Test]
         public void Login()
         {
-
 
             //Change to implicent wait and put into method
             Thread.Sleep(1000);
@@ -73,6 +61,34 @@ namespace BitwardenAutomation
             Thread.Sleep(3000);
 
             Assert.AreEqual(vaultPage.getURL(), "https://testdo.bitwarden.com/#/vault", "URL doesn't match");
+
+        }
+
+        [Test]
+        public void SideBar()
+        {
+            Thread.Sleep(1000);
+
+            LoginPage loginPage = new LoginPage();
+            VaultPage vaultPage = new VaultPage();
+
+            loginPage.EnterUserNameAndPassword(email, password);
+            loginPage.ClickLogin();
+
+            //Change to implicent wait and put into method
+            Thread.Sleep(10000);
+            vaultPage.ClickFavorites();
+            vaultPage.clickTrash();
+            vaultPage.ClickLogin();
+            vaultPage.ClickCard();
+            vaultPage.ClickIdentity();
+
+            //Assert.AreEqual(vaultPage.getURL(), "https://testdo.bitwarden.com/#/vault?type=4", "Identity URL doesn't match");
+            vaultPage.ClickSecureNote();
+            vaultPage.ClickAllItems();
+
+            Assert.AreEqual(vaultPage.getURL(), "https://testdo.bitwarden.com/#/vault", "URL doesn't match");
+
 
         }
     }
